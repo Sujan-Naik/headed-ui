cebf6efd68f5df7834ca8fa2cb4156a55c459721
Sujan Naik <sujannaik047@gmail.com>
2025-07-22 12:24:39 +0100

    Implement calendar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 "use client"
 import {VariantEnum} from "@/components/variants"
 import {useState} from "react";
-import {EventProps, HeadedTimeline} from "@/components/nested/date/Timeline/headed-timeline";
+import {HeadedTimeline} from "@/components/nested/date/Timeline/headed-timeline";
+import {HeadedCalendarMonth} from "@/components/nested/date/Calendar/headed-calendar-month";
+import {HeadedCalendar} from "@/components/nested/date/Calendar/headed-calendar";
+import {EventProps} from "@/components/nested/date/event";
 
 export default function Home() {
   const options = Object.values(VariantEnum)
@@ -20,29 +23,30 @@ const [dropdownOption, SetDropDownOption] = useState<string>('Select an option')
 const [switchState, SetSwitchState] = useState<boolean>(false);
 
 
-// const timelineData: EventProps[] = [
-//     {
-//     variant: VariantEnum.Secondary,
-//     name: "Milestone 1",
-//         date: new Date(2024, 2, 1), // March 1st
-//
-//     description: "Complete design document",
-//   },
-//   {
-//     variant: VariantEnum.Primary,
-//     name: "Project Kickoff",
-//         date: new Date(2024, 2, 10), // March 10th
-//     description: "Start of the project",
-//     endDate: new Date(2024, 2, 15) // March 15th
-//   },
-//
-//   {
-//     variant: VariantEnum.Outline,
-//     name: "Team Meeting",
-//     date: new Date(2024, 2, 20), // March 20th
-//     description: "Weekly team meeting",
-//   },
-// ];
+    const eventData: EventProps[] = [
+        {
+        variant: VariantEnum.Secondary,
+        name: "Milestone 1",
+            date: new Date(2024, 2, 1), // March 1st
+
+        description: "Complete design document",
+      },
+      {
+        variant: VariantEnum.Primary,
+        name: "Project Kickoff",
+            date: new Date(2024, 2, 10), // March 10th
+        description: "Start of the project",
+        endDate: new Date(2024, 2, 15) // March 15th
+      },
+
+      {
+        variant: VariantEnum.Outline,
+        name: "Team Meeting",
+        date: new Date(2024, 2, 20), // March 20th
+        description: "Weekly team meeting",
+      },
+    ];
+
 
 
   return (
@@ -55,8 +59,13 @@ const [switchState, SetSwitchState] = useState<boolean>(false);
 
         {/*<HeadedDatePicker variant={VariantEnum.Primary}/>*/}
 
-        {/*<HeadedTimeline variant={VariantEnum.Primary} events={timelineData} />*/}
-
+        {/*<HeadedTimeline variant={VariantEnum.Primary} events={eventData} />*/}
+        <HeadedCalendar
+            variant={VariantEnum.Primary}
+            initialYear={2024}
+            initialMonth={8} // September (month is 0-based)
+            events={eventData}
+          />
       </div>
   )
   // return (
--- src/components/nested/date/Timeline/headed-timeline.tsx
+++ src/components/nested/date/Timeline/headed-timeline.tsx
@@ -1,30 +1,7 @@
 import {VariantEnum} from "@/components/variants";
 import React from "react";
 import {HeadedCard} from "@/components/unit/content/Card/headed-card";
-
-export interface EventProps{
-    variant: VariantEnum;
-    name: string;
-    description: string;
-    date: Date;
-    endDate?: Date;
-}
-
-const TimelineEvent : React.FC<EventProps> = ({variant, name, description, date, endDate}) => {
-  const options = { year: 'numeric', month: 'short', day: 'numeric' } as const;
-    let startStr = date.toLocaleDateString(undefined, options);
-      if (endDate) {
-        const endStr = endDate.toLocaleDateString(undefined, options);
-        startStr = `${startStr} - ${endStr}`;
-      }
-
-    return <HeadedCard variant={variant}>
-        <h1> {name}</h1>
-        <p> {description}</p>
-        <p> {startStr} </p>
-
-    </HeadedCard>
-}
+import {EventProps, HeadedEvent} from "@/components/nested/date/event";
 
 
 
@@ -40,7 +17,7 @@ export const HeadedTimeline: React.FC<TimelineProps> = ({ variant, events }) => 
   return (
     <>
       {sortedEvents.map((event, index) => (
-        <TimelineEvent key={index} {...event} variant={variant} />
+        <HeadedEvent key={index} {...event} variant={variant} />
       ))}
     </>
   );
